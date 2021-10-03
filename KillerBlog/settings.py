d3d24ed5fabae3c099c120645f1faa2bcd2db61e
Tariq628 <tariqkaleri628@gmail.com>
2021-10-03 12:31:04 +0500

    Some changes in project settings

--- KillerBlog/settings.py
+++ KillerBlog/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -50,12 +50,12 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'DjangoBlog.urls'
+ROOT_URLCONF = 'KillerBlog.urls'
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -68,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'DjangoBlog.wsgi.application'
+WSGI_APPLICATION = 'KillerBlog.wsgi.application'
 
 
 # Database
